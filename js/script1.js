435906445c5ae501224bb8830a15213ea70b38e0
Duc Huy Nguyen <duchuy.nguyen@edu.hefr.ch>
2020-05-14 16:57:15 +0200

    first commit

---
+++ js/script1.js
@@ -0,0 +1,57 @@
+var i = 0;  // start point
+var images = [];
+var time = 5000;
+
+// image list
+images[0] = 'mila-young.jpg';
+images[1] = 'girl.jpg';
+images[2] = 'luca.jpg';
+images[3] = 'nathan.jpg';
+images[4] = 'zen.jpg';
+// images[5] = 'image6.jpeg';
+// images[6] = 'image7.jpeg';
+// images[7] = 'image8.jpeg';
+// images[8] = 'big2.jpg';
+// images[9] = 'big3.jpg';
+
+
+// change image
+function changeImg() {
+    document.slide.src = images[i];
+    if(i < images.length -1){
+        i++;
+    }else{
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
+
+// window.onload = changeImg;
+
+
+function realtimeClock() {
+    var rtClock = new Date();
+
+    var hour = rtClock.getHours();
+    var minute = rtClock.getMinutes();
+    var seconde = rtClock.getSeconds();
+
+    var amPm = (hour < 12) ? "AM" : "PM";
+    hour = (hour > 12) ? hour -12 : hour;
+
+    hour = ("0" + hour).slice(-2);
+    minute = ("0" + minute).slice(-2);
+    seconde = ("0" + seconde).slice(-2);
+
+    document.getElementById('clock').innerHTML=
+        hour + " : " + minute + " : " + seconde + " " + amPm ;
+    var t = setTimeout(realtimeClock, 500);
+
+}
+
+function start() {
+    realtimeClock();
+    changeImg();
+}
+
